c5869dbf64eabaa33f33467145759ac6c37d80b1
Mari Kambe <kambe.m1227@gmail.com>
2020-05-10 11:10:36 +0900

    APIを用いたqiita記事の検索機能を実装

--- js/main.js
+++ js/main.js
@@ -100,3 +100,50 @@ var taxApp = new Vue({
     }
   }
 })
+
+var qiitaSearch = new Vue({
+  el: '#qiita_search',
+  data: {
+    items: null,
+    keyword: '',
+    message: ''
+  },
+  watch: {
+    keyword: function(newKeyword, oldKeyword) {
+      // console.log(newKeyword)
+      this.message = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
+    }
+
+  },
+  created: function() {
+    // this.keyword = 'Javascript'
+    // this.getAnswer()
+    // 1000ミリ秒内に同じイベントが発生する場合は処理を実行しない
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+  },
+  methods: {
+    getAnswer: function() {
+      if(this.keyword === '') {
+        this.items = null
+        this.message = ''
+        return
+      }
+
+      this.message = 'Loading...'
+      var vm = this
+      var params = { page: 1, per_page: 20, query: this.keyword }
+      axios.get('https://qiita.com/api/v2/items', { params })
+        .then(function(response){
+          console.log(response)
+          vm.items = response.data
+        })
+        .catch(function(error) {
+          vm.message = 'Error!' + error
+        })
+        .finally(function(){
+          vm.message = ''
+        })
+    }
+  }
+})
